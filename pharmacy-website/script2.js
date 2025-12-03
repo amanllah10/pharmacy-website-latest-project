1ecce35ca1c20e12a9891ee42c8ff17df291482c
amanllah10 <amanullah107676@gmail.com>
2025-12-02 21:09:34 -0800

    ok

--- pharmacy-website/script2.js
+++ pharmacy-website/script2.js
@@ -12,20 +12,40 @@ buyTitle.innerHTML = localStorage.getItem('producttitle');
 alt.innerHTML = localStorage.getItem('productalt');
 
 // Lower images switch
-const altValue = alt.innerHTML;
-const imageSets = {
-    relief: ['/images/DSC00001.JPG', '/images/DSC00007.JPG', '/images/DSC00008.JPG'],
-    vitamin: ['/images/DSC00033.JPG', '/images/DSC00037.JPG', '/images/DSC00042.JPG'],
-    joint: ['/images/DSC00053.JPG', '/images/DSC00055.JPG', '/images/DSC00056.JPG'],
-    carica: ['/images/DSC00058.JPG', '/images/DSC00061.JPG', '/images/DSC00066.JPG'],
-    cough: ['/images/DSC00072.JPG', '/images/DSC00075.JPG', '/images/DSC00075.JPG'],
-    pain: ['/images/DSC00077.JPG', '/images/DSC00086.JPG', '/images/DSC00087.JPG'],
-    default: ['/images/DSC00017.JPG', '/images/DSC00017.JPG', '/images/DSC00017.JPG']
-};
-let selectedImages = imageSets[altValue] || imageSets.default;
-img2.src = selectedImages[0];
-img3.src = selectedImages[1];
-img4.src = selectedImages[2];
+// if else operation for styling lower 3 images
+if (alt.innerHTML === 'relief') {
+    img2.src = '../images/DSC00001.JPG'
+    img3.src = '../images/DSC00007.JPG'
+    img4.src = '../images/DSC00008.JPG'
+} else if (alt.innerHTML === 'vitamin') {
+    img2.src = '../images/DSC00033.JPG'
+    img3.src = '../images/DSC00037.JPG'
+    img4.src = '../images/DSC00042.JPG'
+} else if (alt.innerHTML === 'joint') {
+    img2.src = '../images/DSC00053.JPG'
+    img3.src = '../images/DSC00055.JPG'
+    img4.src = '../images/DSC00056.JPG'
+} else if (alt.innerHTML === 'carica') {
+    img2.src = '../images/DSC00058.JPG'
+    img3.src = '../images/DSC00061.JPG'
+    img4.src = '../images/DSC00066.JPG'
+} else if (alt.innerHTML === 'cough') {
+    img2.src = '../images/DSC00072.JPG'
+    img3.src = '../images/DSC00075.JPG'
+    img4.src = '../images/DSC00075.JPG'
+} else if (alt.innerHTML === 'pain') {
+    img2.src = '../images/DSC00077.JPG'
+    img3.src = '../images/DSC00086.JPG'
+    img4.src = '../images/DSC00087.JPG'
+} else if (alt.innerHTML === 'skin') {
+    img2.src = '../images/DSC00017.JPG'
+    img3.src = '../images/DSC00017.JPG'
+    img4.src = '../images/DSC00017.JPG'
+} else {
+    img2.src = '../images/DSC00017.JPG'
+    img3.src = '../images/DSC00017.JPG'
+    img4.src = '../images/DSC00017.JPG'
+}
 
 // ================== QUANTITY ==================
 let plusBtn = document.querySelector('.plus');
